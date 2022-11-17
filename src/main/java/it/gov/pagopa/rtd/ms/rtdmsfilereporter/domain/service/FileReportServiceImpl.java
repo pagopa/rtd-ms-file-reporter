85b030e75e11f637a2cefb89c67fad3ef1ebf576
Andrea Morabito <andrea.morabito@pagopa.it>
2022-11-17 18:08:50 +0100

    [RTD-882] add service for query use case

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/domain/service/FileReportServiceImpl.java
@@ -0,0 +1,23 @@
+package it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.service;
+
+import static it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.FileReportAggregator.aggregateFileReports;
+
+import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.FileReport;
+import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.repository.FileReportRepository;
+import java.util.Collection;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class FileReportServiceImpl implements FileReportService {
+
+  private final FileReportRepository fileReportRepository;
+
+  @Override
+  public FileReport getFileReport(Collection<String> senderCodes) {
+    return fileReportRepository.getReportsBySenderCodes(senderCodes)
+        .stream()
+        .collect(aggregateFileReports());
+  }
+}
