c836401849e24864421cb2861d53cecb54ee1a8d
Andrea Morabito <andrea.morabito@pagopa.it>
2022-11-29 18:19:59 +0100

    [RTD-1017] remove transactional annotation

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/domain/repository/FileReportRepository.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/domain/repository/FileReportRepository.java
@@ -3,12 +3,10 @@ package it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.repository;
 
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.FileReport;
 import java.util.Collection;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface FileReportRepository {
 
   Collection<FileReport> getReportsBySenderCodes(Collection<String> senderCodes);
 
-  @Transactional
   void save(FileReport fileReport);
 }
