b41ec12a1677f02fbaa8e8a018d3d63dfce8b468
Andrea Morabito <andrea.morabito@pagopa.it>
2022-11-30 15:22:46 +0100

    [RTD-886] remove useless annotation

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/event/EventHandler.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/event/EventHandler.java
@@ -2,7 +2,6 @@ package it.gov.pagopa.rtd.ms.rtdmsfilereporter.event;
 
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.event.model.ProjectorEventDto;
 import java.util.function.Consumer;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +12,6 @@ import org.springframework.messaging.Message;
  */
 @Slf4j
 @Configuration
-@Getter
 public class EventHandler {
 
   @Bean
