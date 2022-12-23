90e0c717c4998de4ca6ff532585c647cb42c7e43
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-12-23 12:02:59 +0100

    [RTD-1101] add json serializer to receiveTimestamp of ProjectorEventDto

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/event/model/ProjectorEventDto.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/event/model/ProjectorEventDto.java
@@ -1,7 +1,9 @@
 package it.gov.pagopa.rtd.ms.rtdmsfilereporter.event.model;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -29,6 +31,7 @@ public class ProjectorEventDto {
 
   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
   @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
   @NotNull
   private LocalDateTime receiveTimestamp;
 
