f1a73f587dddc26f7eb06e7f2ad4c828f784ead3
Andrea Morabito <andrea.morabito@pagopa.it>
2023-05-29 10:58:53 +0200

    [RTD-1042] add controller and business logic for ade ack list retrieve

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/controller/FileReportController.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/controller/FileReportController.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller;
 
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller.model.FileReportDto;
+import it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller.model.SenderAdeAckListDto;
+import jakarta.validation.constraints.NotNull;
 import java.util.Collection;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,4 +12,8 @@ public interface FileReportController {
 
   @GetMapping(value = "file-report", produces = MediaType.APPLICATION_JSON_VALUE)
   FileReportDto getFileReport(@RequestParam(name = "senderCodes") Collection<String> senderCodes);
+
+  @GetMapping(value = "/sender-ade-ack", produces = MediaType.APPLICATION_JSON_VALUE)
+  SenderAdeAckListDto getSenderAdeAckList(
+      @NotNull @RequestParam(name = "senders") Collection<String> senderCodes);
 }
