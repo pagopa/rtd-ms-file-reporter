9c1375b18d5f700855936186ac4fc03098d38be8
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2023-05-29 15:06:48 +0200

    [RTD-1042] add controller and business logic for ade ack list retrieve (#57)

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/controller/FileReportControllerImpl.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/controller/FileReportControllerImpl.java
@@ -2,7 +2,9 @@ package it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller;
 
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller.model.FileReportDto;
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller.model.FileReportDtoMapper;
+import it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller.model.SenderAdeAckListDto;
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.service.FileReportService;
+import jakarta.validation.constraints.NotNull;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +22,15 @@ public class FileReportControllerImpl implements FileReportController {
   @Override
   public FileReportDto getFileReport(Collection<String> senderCodes) {
     log.info("GET file report for sender codes: {}", senderCodes.toString());
-    return modelMapper.map(fileReportService.getAggregateFileReport(senderCodes), FileReportDto.class);
+    return modelMapper.map(fileReportService.getAggregateFileReport(senderCodes),
+        FileReportDto.class);
+  }
+
+  @Override
+  public SenderAdeAckListDto getSenderAdeAckList(@NotNull Collection<String> senderCodes) {
+    log.info("GET ack to download list for sender codes: {}", senderCodes.toString());
+    return SenderAdeAckListDto.builder()
+        .fileNameList(fileReportService.getAckToDownloadList(senderCodes))
+        .build();
   }
 }
