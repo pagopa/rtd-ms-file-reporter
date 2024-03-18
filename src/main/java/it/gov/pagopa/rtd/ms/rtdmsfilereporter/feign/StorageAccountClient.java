22594ef71853c7de764783071cfb1cf68a2161d4
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-18 18:12:09 +0100

    typo

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/StorageAccountClient.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/StorageAccountClient.java
@@ -1,7 +1,9 @@
 package it.gov.pagopa.rtd.ms.rtdmsfilereporter.feign;
 
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.AggregatesDataSummary;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StorageAccountClient {
 
   public AggregatesDataSummary getMetadata(String blobPath) {
