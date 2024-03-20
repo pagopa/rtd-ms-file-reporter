10d697ce478bd26c1576dc3e9108f03981bf3be4
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-20 09:47:33 +0100

    add constructors on AggregatesDataSummary

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/domain/model/AggregatesDataSummary.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/domain/model/AggregatesDataSummary.java
@@ -1,11 +1,15 @@
 package it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AggregatesDataSummary {
 
   private LocalDateTime minAccountingDate;
