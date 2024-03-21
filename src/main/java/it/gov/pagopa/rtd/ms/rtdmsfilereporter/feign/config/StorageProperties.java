c3e8f64c1d1d08899685aecd00513d360c85a4fc
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-21 11:11:13 +0100

    remove configuration annotation from properties

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/StorageProperties.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/StorageProperties.java
@@ -3,10 +3,8 @@ package it.gov.pagopa.rtd.ms.rtdmsfilereporter.feign.config;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "spring.cloud.openfeign.client.config.StorageAccountConnector")
-@Configuration
 public record StorageProperties
     (@NotNull
      @NotBlank
@@ -14,4 +12,5 @@ public record StorageProperties
      @NotNull
      @NotBlank
      String apiKey) {
+
 }
