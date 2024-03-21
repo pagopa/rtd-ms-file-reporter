a2b70300e7c7d1b9a4b7a2027054900fa2383049
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-21 12:07:17 +0100

    configure ssl context for apache client

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/FeignClientConfig.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/FeignClientConfig.java
@@ -1,7 +1,22 @@
 package it.gov.pagopa.rtd.ms.rtdmsfilereporter.feign.config;
 
 import feign.RequestInterceptor;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.SSLContext;
 import lombok.RequiredArgsConstructor;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.TrustSelfSignedStrategy;
+import org.apache.hc.core5.http.config.Registry;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
 import org.springframework.context.annotation.Bean;
 
 @RequiredArgsConstructor
@@ -15,4 +30,24 @@ public class FeignClientConfig {
     return requestTemplate -> requestTemplate.header(
         "Ocp-Apim-Subscription-Key", properties.apiKey());
   }
+
+  @Bean
+  CloseableHttpClient myHttpClient()
+      throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    SSLContext sslContext = SSLContexts.custom()
+        .loadTrustMaterial(TrustSelfSignedStrategy.INSTANCE)
+        .build();
+
+    Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+        .register("http", PlainConnectionSocketFactory.INSTANCE)
+        .register("https",
+            new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE))
+        .build();
+
+    PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(
+        registry);
+
+    return HttpClients.custom().setConnectionManager(connectionManager).build();
+  }
+
 }
