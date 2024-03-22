8ce5836e470512facdf0f0cd93b539a3363f7680
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-22 09:05:04 +0100

    change properties position

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/StorageProperties.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/StorageProperties.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "spring.cloud.openfeign.client.config.storage-account-connector")
+@ConfigurationProperties(prefix = "storage-account-connector")
 public record StorageProperties
     (@NotNull
      @NotBlank
