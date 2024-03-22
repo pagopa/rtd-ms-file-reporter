87f88e2f3c987bbd36ecce159ce9180f138236d4
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-22 14:53:31 +0100

    restore command behaviour

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/domain/service/DecryptedEventCommand.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/domain/service/DecryptedEventCommand.java
@@ -1,10 +1,8 @@
 package it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.service;
 
-import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.AggregatesDataSummary;
-import it.gov.pagopa.rtd.ms.rtdmsfilereporter.feign.StorageAccountService;
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.FileMetadata;
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.FileReport;
-import java.io.IOException;
+import it.gov.pagopa.rtd.ms.rtdmsfilereporter.feign.StorageAccountService;
 import java.util.function.BiConsumer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -18,12 +16,7 @@ public class DecryptedEventCommand implements BiConsumer<FileReport, FileMetadat
   @Override
   public void accept(FileReport fileReport, FileMetadata fileMetadata) {
     // preliminary api call
-    AggregatesDataSummary dataSummary = null;
-    try {
-      dataSummary = client.getMetadata(fileMetadata.getPath(), fileMetadata.getName());
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    var dataSummary = client.getMetadata(fileMetadata.getPath(), fileMetadata.getName());
     // actions on domain object
     fileMetadata.enrichWithSquaringData(dataSummary);
     // two operations are needed: update status + add aggregates summary
