41dad2385fd230dce6da8cdb53f34e9547978e3a
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-22 14:54:00 +0100

    cleanup comments and imports

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/RtdMsFileReporterApplication.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/RtdMsFileReporterApplication.java
@@ -1,19 +1,16 @@
 package it.gov.pagopa.rtd.ms.rtdmsfilereporter;
 
-import it.gov.pagopa.rtd.ms.rtdmsfilereporter.feign.StorageAccountRestConnector;
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.feign.config.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableFeignClients(clients = StorageAccountRestConnector.class)
 @EnableConfigurationProperties(StorageProperties.class)
 public class RtdMsFileReporterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RtdMsFileReporterApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RtdMsFileReporterApplication.class, args);
+  }
 
 }
