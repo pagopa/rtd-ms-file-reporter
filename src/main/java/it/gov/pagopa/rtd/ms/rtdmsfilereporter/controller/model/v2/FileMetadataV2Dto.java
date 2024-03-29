69c3b584c0f3ecbb18e5f434077eb08052ef2bee
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-29 16:04:48 +0100

    remove code duplication

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/controller/model/v2/FileMetadataV2Dto.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/controller/model/v2/FileMetadataV2Dto.java
@@ -1,33 +1,14 @@
 package it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller.model.v2;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller.model.v1.FileMetadataDto;
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.AggregatesDataSummary;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class FileMetadataV2Dto {
-
-  @NotNull
-  @NotBlank
-  private String name;
-
-  private Long size;
-
-  @NotNull
-  @NotBlank
-  private String status;
-
-  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
-  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-  private LocalDateTime transmissionDate;
+public class FileMetadataV2Dto extends FileMetadataDto {
 
   @NotNull
   private AggregatesDataSummary dataSummary;
