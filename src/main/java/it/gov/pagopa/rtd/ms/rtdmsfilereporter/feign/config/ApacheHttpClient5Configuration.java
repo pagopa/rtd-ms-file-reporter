c9620bf3416111bb85bcc4266b79948fdd222059
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-05-24 17:49:56 +0200

    chore: [RTD-2488] fix

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/ApacheHttpClient5Configuration.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/ApacheHttpClient5Configuration.java
@@ -2,11 +2,13 @@ package it.gov.pagopa.rtd.ms.rtdmsfilereporter.feign.config;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.apachehttpclient.v5_2.ApacheHttpClient5Telemetry;
+import lombok.RequiredArgsConstructor;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApacheHttpClient5Configuration {
 
   private OpenTelemetry openTelemetry;
