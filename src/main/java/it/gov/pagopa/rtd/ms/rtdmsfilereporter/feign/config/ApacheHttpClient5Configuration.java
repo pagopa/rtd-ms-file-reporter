27267fea428598cb2e8a0c88b18ead83467a5886
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-05-24 16:14:02 +0200

    chore: [RTD-2488] use instrumented client for HTTP client builder

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/ApacheHttpClient5Configuration.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/ApacheHttpClient5Configuration.java
@@ -2,7 +2,6 @@ package it.gov.pagopa.rtd.ms.rtdmsfilereporter.feign.config;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.apachehttpclient.v5_2.ApacheHttpClient5Telemetry;
-import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +11,9 @@ public class ApacheHttpClient5Configuration {
 
   private OpenTelemetry openTelemetry;
 
-  // creates a new http client with open telemetry instrumentation
+  // creates a new http client builder for constructing http clients with open telemetry instrumentation
   @Bean
-  public HttpClient newHttpClient() {
-    return ApacheHttpClient5Telemetry.builder(openTelemetry).build().newHttpClient();
+  public HttpClientBuilder createHttpClientBuilder() {
+    return ApacheHttpClient5Telemetry.builder(openTelemetry).build().newHttpClientBuilder();
   }
 }
--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/HttpClientConfig.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/HttpClientConfig.java
@@ -1,11 +1,11 @@
 package it.gov.pagopa.rtd.ms.rtdmsfilereporter.feign.config;
 
+import io.opentelemetry.api.OpenTelemetry;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import javax.net.ssl.SSLContext;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
 import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
@@ -21,8 +21,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HttpClientConfig {
 
+  private OpenTelemetry openTelemetry;
+
+  private ApacheHttpClient5Configuration apacheHttpClient5Configuration;
+
   @Bean
-  CloseableHttpClient getHttpClient()
+  HttpClient getHttpClient()
       throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
     SSLContext sslContext = SSLContexts.custom()
         .loadTrustMaterial(TrustSelfSignedStrategy.INSTANCE)
@@ -37,6 +41,7 @@ public class HttpClientConfig {
     PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(
         registry);
 
-    return HttpClientBuilder.create().setConnectionManager(connectionManager).build();
+    return apacheHttpClient5Configuration.createHttpClientBuilder().setConnectionManager(connectionManager)
+        .build();
   }
 }
