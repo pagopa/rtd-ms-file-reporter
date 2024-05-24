be5addaa24853fc3b0c411c214e17d56baad48c3
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-05-24 11:34:08 +0200

    chore: [RTD-2488] make HttpClient and HttpClientBuilder beans

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/ApacheHttpClient5Configuration.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/feign/config/ApacheHttpClient5Configuration.java
@@ -4,22 +4,19 @@ import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.apachehttpclient.v5_2.ApacheHttpClient5Telemetry;
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
 
 public class ApacheHttpClient5Configuration {
 
-  private OpenTelemetry openTelemetry;
-
-  public ApacheHttpClient5Configuration(OpenTelemetry openTelemetry) {
-    this.openTelemetry = openTelemetry;
-  }
-
   // creates a new http client builder for constructing http clients with open telemetry instrumentation
-  public HttpClientBuilder createBuilder() {
+  @Bean
+  public HttpClientBuilder createBuilder(OpenTelemetry openTelemetry) {
     return ApacheHttpClient5Telemetry.builder(openTelemetry).build().newHttpClientBuilder();
   }
 
   // creates a new http client with open telemetry instrumentation
-  public HttpClient newHttpClient() {
+  @Bean
+  public HttpClient newHttpClient(OpenTelemetry openTelemetry) {
     return ApacheHttpClient5Telemetry.builder(openTelemetry).build().newHttpClient();
   }
 }
