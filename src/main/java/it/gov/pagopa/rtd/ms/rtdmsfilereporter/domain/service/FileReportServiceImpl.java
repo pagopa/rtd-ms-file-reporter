276ac35638e1a6cee147ed7ff9b6d167ff492ab4
Michele Fucito <michele.fucito@digitouch.it>
2025-02-10 14:24:28 +0100

    fix: [TAE-242] fix log error

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/domain/service/FileReportServiceImpl.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/domain/service/FileReportServiceImpl.java
@@ -17,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-
+@Service
 @Slf4j
 @RequiredArgsConstructor
 public class FileReportServiceImpl implements FileReportService {
