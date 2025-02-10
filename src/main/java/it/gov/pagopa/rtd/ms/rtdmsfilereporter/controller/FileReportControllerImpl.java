b47171c0f4261c9e382c1a909c326b42ef9ee265
Michele <michele.fucito@mobilesoft.it>
2025-02-10 10:53:18 +0100

    fix: TAE-242 add tests

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/controller/FileReportControllerImpl.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/controller/FileReportControllerImpl.java
@@ -7,9 +7,13 @@ import it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller.model.v2.FileReportV2Dt
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.controller.model.v2.FileReportV2DtoMapper;
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.service.FileReportService;
 import jakarta.validation.constraints.NotNull;
+
 import java.util.Collection;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,34 +21,38 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class FileReportControllerImpl implements FileReportController {
 
-  private final FileReportService fileReportService;
-  private final FileReportDtoMapper mapper;
-  private final FileReportV2DtoMapper mapperV2;
+    private final FileReportService fileReportService;
+    private final FileReportDtoMapper mapper;
+    private final FileReportV2DtoMapper mapperV2;
 
-  @Override
-  public FileReportDto getFileReport(Collection<String> senderCodes) {
-    log.info("GET file report for sender codes: {}", senderCodes.toString());
-    return mapper.fileReportToDto(fileReportService.getAggregateFileReport(senderCodes));
-  }
+    @Override
+    public FileReportDto getFileReport(Collection<String> senderCodes) {
+        log.info("GET file report for sender codes: {}", senderCodes.toString());
+        return mapper.fileReportToDto(fileReportService.getAggregateFileReport(senderCodes));
+    }
 
-  @Override
-  public FileReportV2Dto getFileReportV2(Collection<String> senderCodes) {
-    log.info("GET file report v2 for sender codes: {}", senderCodes.toString());
-    return mapperV2.fileReportToDto(fileReportService.getAggregateFileReport(senderCodes));
-  }
+    @Override
+    public FileReportV2Dto getFileReportV2(Collection<String> senderCodes) {
+        log.info("GET file report v2 for sender codes: {}", senderCodes.toString());
+        return mapperV2.fileReportToDto(fileReportService.getAggregateFileReport(senderCodes));
+    }
 
-  @Override
-  public SenderAdeAckListDto getSenderAdeAckList(@NotNull Collection<String> senderCodes) {
-    log.info("GET ack to download list for sender codes: {}", senderCodes.toString());
-    return SenderAdeAckListDto.builder()
-        .fileNameList(fileReportService.getAckToDownloadList(senderCodes))
-        .build();
-  }
+    @Override
+    public SenderAdeAckListDto getSenderAdeAckList(@NotNull Collection<String> senderCodes) {
+        log.info("GET ack to download list for sender codes: {}", senderCodes.toString());
+        return SenderAdeAckListDto.builder()
+                .fileNameList(fileReportService.getAckToDownloadList(senderCodes))
+                .build();
+    }
 
-  @Override
-  public void getMetadata(@NotNull String basePath, @NotNull String fileName) {
-    String sanitizedFileName = fileName.replace("\n", "").replace("\r", "");
-    log.info("Enrich metadata on file : {}", sanitizedFileName);
-    fileReportService.getMetadata(basePath, fileName);
-  }
+    @Override
+    @GetMapping("/v1/report/metadata")
+    public void getMetadata(@RequestParam @NotNull String basePath,
+                            @RequestParam @NotNull String fileName) {
+        String sanitizedFileName = fileName.replace("\n", "").replace("\r", "");
+        log.info("Enrich metadata on file : {}", sanitizedFileName);
+        fileReportService.getMetadata(basePath, fileName);
+    }
+
+
 }
--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/service/FileReportServiceImplTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsfilereporter/service/FileReportServiceImplTest.java
@@ -2,18 +2,25 @@ package it.gov.pagopa.rtd.ms.rtdmsfilereporter.service;
 
 import static it.gov.pagopa.rtd.ms.rtdmsfilereporter.TestUtils.createFileReport;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
+import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.FileMetadata;
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.FileReport;
+import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.model.AggregatesDataSummary;
+
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.repository.FileReportRepository;
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.service.FileReportService;
 import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.service.FileReportServiceImpl;
+import it.gov.pagopa.rtd.ms.rtdmsfilereporter.domain.service.StorageAccountService;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,93 +28,148 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.web.server.ResponseStatusException;
 
 class FileReportServiceImplTest {
 
-  @Mock
-  FileReportRepository fileReportRepository;
-  FileReportService fileReportService;
-  AutoCloseable autoCloseable;
+    @Mock
+    FileReportRepository fileReportRepository;
 
-  @BeforeEach
-  void setUp() {
-    autoCloseable = MockitoAnnotations.openMocks(this);
+    @Mock
+    StorageAccountService storageAccountService;
 
-    fileReportService = new FileReportServiceImpl(fileReportRepository, any());
-  }
+    FileReportService fileReportService;
+    AutoCloseable autoCloseable;
 
-  @SneakyThrows
-  @AfterEach
-  void tearDown() {
-    autoCloseable.close();
-  }
 
-  @Test
-  void whenGetFileReportForManySenderCodesThenMergeTheReportsCorrectly() {
-    Mockito.when(fileReportRepository.getReportsBySenderCodes(any())).thenReturn(getReportList());
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
 
-    FileReport filereport = fileReportService.getAggregateFileReport(
-        Collections.singleton("12345"));
+        fileReportService = new FileReportServiceImpl(fileReportRepository, storageAccountService);
+    }
 
-    assertThat(filereport).isNotNull();
-    assertThat(filereport.getFilesUploaded()).isNotNull().hasSize(4);
-    assertThat(filereport.getAckToDownload()).isNotNull().hasSize(6);
-  }
+    @SneakyThrows
+    @AfterEach
+    void tearDown() {
+        autoCloseable.close();
+    }
 
-  @Test
-  void givenEmptyListWhenGetFileReportForManySenderCodesThenReturnsEmptyReport() {
-    Mockito.when(fileReportRepository.getReportsBySenderCodes(any()))
-        .thenReturn(Collections.emptyList());
+    @Test
+    void whenGetFileReportForManySenderCodesThenMergeTheReportsCorrectly() {
+        Mockito.when(fileReportRepository.getReportsBySenderCodes(any())).thenReturn(getReportList());
 
-    FileReport filereport = fileReportService.getAggregateFileReport(
-        Collections.singleton("12345"));
+        FileReport filereport = fileReportService.getAggregateFileReport(
+                Collections.singleton("12345"));
 
-    assertThat(filereport).isNotNull();
-    assertThat(filereport.getFilesUploaded()).isNotNull().isEmpty();
-    assertThat(filereport.getAckToDownload()).isNotNull().isEmpty();
-  }
+        assertThat(filereport).isNotNull();
+        assertThat(filereport.getFilesUploaded()).isNotNull().hasSize(4);
+        assertThat(filereport.getAckToDownload()).isNotNull().hasSize(6);
+    }
 
-  @Test
-  void whenGetFileReportThenReturnsAReport() {
-    Mockito.when(fileReportRepository.getReportBySenderCode(any()))
-        .thenReturn(Optional.empty());
+    @Test
+    void givenEmptyListWhenGetFileReportForManySenderCodesThenReturnsEmptyReport() {
+        Mockito.when(fileReportRepository.getReportsBySenderCodes(any()))
+                .thenReturn(Collections.emptyList());
 
-    var filereport = fileReportService.getFileReport("12345");
+        FileReport filereport = fileReportService.getAggregateFileReport(
+                Collections.singleton("12345"));
 
-    assertThat(filereport).isEmpty();
-  }
+        assertThat(filereport).isNotNull();
+        assertThat(filereport.getFilesUploaded()).isNotNull().isEmpty();
+        assertThat(filereport.getAckToDownload()).isNotNull().isEmpty();
+    }
 
-  @Test
-  void whenSaveThenRepositorySaveIsInvoked() {
-    fileReportService.save(FileReport.createFileReport());
+    @Test
+    void whenGetFileReportThenReturnsAReport() {
+        Mockito.when(fileReportRepository.getReportBySenderCode(any()))
+                .thenReturn(Optional.empty());
 
-    verify(fileReportRepository).save(FileReport.createFileReport());
-  }
+        var filereport = fileReportService.getFileReport("12345");
 
-  @Test
-  void whenGetAckToDownloadThenReturnsACollectionOfStrings() {
-    var reportMock = getReportList();
-    Mockito.when(fileReportRepository.getReportsBySenderCodes(any())).thenReturn(reportMock);
+        assertThat(filereport).isEmpty();
+    }
 
-    var ackToDownloadList = fileReportService.getAckToDownloadList(Collections.singleton("12345"));
+    @Test
+    void whenSaveThenRepositorySaveIsInvoked() {
+        fileReportService.save(FileReport.createFileReport());
 
-    var expectedList = reportMock.stream()
-        .map(FileReport::getAckToDownload)
-        .flatMap(Collection::stream)
-        .collect(Collectors.toSet());
-    assertThat(ackToDownloadList)
-        .hasSize(expectedList.size())
-        .containsExactlyInAnyOrderElementsOf(expectedList);
-  }
+        verify(fileReportRepository).save(FileReport.createFileReport());
+    }
 
-  @Test
-  void whenGetMetadataThenSaveFileReport(){
+    @Test
+    void whenGetAckToDownloadThenReturnsACollectionOfStrings() {
+        var reportMock = getReportList();
+        Mockito.when(fileReportRepository.getReportsBySenderCodes(any())).thenReturn(reportMock);
 
-  }
+        var ackToDownloadList = fileReportService.getAckToDownloadList(Collections.singleton("12345"));
 
-  Collection<FileReport> getReportList() {
-    return Stream.of(createFileReport(3, 1), createFileReport(1, 2), createFileReport(2, 1))
-        .collect(Collectors.toList());
-  }
+        var expectedList = reportMock.stream()
+                .map(FileReport::getAckToDownload)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+        assertThat(ackToDownloadList)
+                .hasSize(expectedList.size())
+                .containsExactlyInAnyOrderElementsOf(expectedList);
+    }
+
+    @Test
+    void whenGetMetadataThenSaveFileReport() {
+
+    }
+
+    Collection<FileReport> getReportList() {
+        return Stream.of(createFileReport(3, 1), createFileReport(1, 2), createFileReport(2, 1))
+                .collect(Collectors.toList());
+    }
+
+
+    @Test
+    void whenGetMetadata_givenWellFormedFilename_thenExtractSenderCodeAndSave() {
+        String basePath = "basePath";
+        String fileName = "ADE.12345.TRNLOG.20230101.130000.001.01.csv";
+        String senderCode = "12345";
+
+        FileReport fileReport = FileReport.createFileReportWithSenderCode(senderCode);
+
+        var fileMetadata = new FileMetadata();
+        fileMetadata.setName(fileName);
+        fileMetadata.setTransmissionDate(java.time.LocalDateTime.now());
+        fileReport.addFileOrUpdateStatusIfPresent(fileMetadata);
+
+        Mockito.when(fileReportRepository.getReportBySenderCode(senderCode))
+                .thenReturn(Optional.of(fileReport));
+
+        AggregatesDataSummary dataSummaryMock = AggregatesDataSummary.builder()
+                .countPositiveTransactions(100)
+                .sumAmountPositiveTransactions(1000)
+                .build();
+        Mockito.when(storageAccountService.getMetadata("/" + basePath + "/", fileName))
+                .thenReturn(dataSummaryMock);
+
+        fileReportService.getMetadata(basePath, fileName);
+
+        Mockito.verify(fileReportRepository).save(fileReport);
+        assertThat(fileMetadata.getPath()).isEqualTo("/" + basePath + "/");
+    }
+
+    @Test
+    void whenGetMetadata_givenNoMatchingFileMetadata_thenThrowNotFound() {
+
+        String basePath = "basePath";
+        String fileName = "ADE.12345.TRNLOG.20230101.130000.001.01.csv";
+        String senderCode = "12345";
+
+        FileReport fileReport = FileReport.createFileReportWithSenderCode(senderCode);
+
+        Mockito.when(fileReportRepository.getReportBySenderCode(senderCode))
+                .thenReturn(Optional.of(fileReport));
+
+        assertThatThrownBy(() -> fileReportService.getMetadata(basePath, fileName))
+                .isInstanceOf(ResponseStatusException.class)
+                .hasMessageContaining("FileMetadata not found in FileReport");
+
+        Mockito.verify(fileReportRepository, Mockito.never()).save(any());
+    }
 
 }
